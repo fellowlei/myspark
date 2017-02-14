42af7802ac56cba289bd51d385535b240910a7c2
lulei11 <lulei@jd.com>
2017-02-14 15:03:40 +0800

    test

---
+++ src/main/resources/shell/install-phantomjs.sh
@@ -0,0 +1,41 @@
+#http://phantomjs.org/
+#phantomjs study
+cd /home/hadoop/sw
+#wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
+tar -jxvf phantomjs-2.1.1-linux-x86_64.tar.bz2 -C /usr/local
+cd /usr/local
+mv phantomjs-2.1.1-linux-x86_64/ phantomjs
+echo """export PHANTOMJS_HOME=/usr/local/phantomjs/
+export PATH=\$PATH:\$PHANTOMJS_HOME/bin
+""" >> /etc/profile
+
+source /etc/profile
+
+#test
+#http://phantomjs.org/screen-capture.html
+cd /usr/local/phantomjs
+mkdir mycode
+cd /usr/local/phantomjs/mycode
+echo """var page = require('webpage').create();
+page.open('http://github.com/', function() {
+  page.render('github.png');
+  phantom.exit();
+});
+""" > github.js
+echo "gen github.js success"
+
+echo "start test capture..."
+phantomjs github.js
+echo "gen github png success"
+cd /usr/local/phantomjs/examples
+#gen png
+phantomjs rasterize.js http://ariya.github.io/svg/tiger.svg tiger.png
+echo "gen tiger.png success"
+#gen png
+phantomjs rasterize.js https://dmitrybaranovskiy.github.io/raphael/polar-clock.html clock.png
+echo "gen clock.png success"
+
+#gen pdf
+phantomjs rasterize.js 'http://en.wikipedia.org/w/index.php?title=Jakarta&printable=yes' jakarta.pdf
+echo "gen jakarta.pdf success"
+
